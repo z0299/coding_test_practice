23cff9b4e260cebe8598c766f5f632072a2059e0
jiyoungKim <rlawldud.kim99@gmail.com>
2022-07-29 17:27:47 +0900

    greedy_bj1783 병든 나이트

---
+++ bj1783.py
@@ -0,0 +1,15 @@
+#greedy 알고리즘 공부 - #1783 병든 나이트
+
+#case별로 쪼개어 생각해보아야함! case쪼개는게 중요...
+#첫번째 시작점부터 1칸으로 치는 것 주의!
+
+n, m = map(int,input().split())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(min((m-1)//2+1, 4))
+elif m < 7:
+    print(min(m, 4))
+elif n >= 3 and m >= 7:
+    print(5+(m-7))
