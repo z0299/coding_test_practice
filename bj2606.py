bda3da772bbca2858e563f3b822ee32c5482ef0a
jiyoungKim <rlawldud.kim99@gmail.com>
2022-08-01 12:32:39 +0900

    brute-force_bj2606 바이러스

--- bj2606.py
+++ bj2606.py
@@ -1,8 +1,10 @@
 #완전탐색 알고리즘 공부 - #2606 바이러스
 #BFS
+#DFS로도 풀 수 있다.
 import sys
 from collections import deque
 input = sys.stdin.readline
+cnt = 0 #글로벌 변수 선언 시 가장 앞에 선언
 
 def BFS():
     queue = deque()
@@ -13,7 +15,15 @@ def BFS():
             if not virus[i] and network_list[i] == 1:
                 queue.append(network[i])
                 virus[i] = True
-    print(virus.count(True)-1)
+    print(virus.count(True)-1)  #count: virus 리스트에서 True의 개수 구하기
+
+def DFS(start):
+    global cnt  #함수 안에서 전역 변수의 값을 변경하려면 global키워드로 선언해주어야 한다.
+    virus[start] = True
+    for i in range(1, node_num+1):
+        if not virus[i] and network[start][i] == 1:
+            DFS(i)
+            cnt += 1
 
 node_num = int(input())
 network_num = int(input())
@@ -27,3 +37,5 @@ for _ in range(network_num):
 virus = [False]*(node_num+1)
 
 BFS()
+DFS(1)
+print(cnt)
