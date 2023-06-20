7f01fd52d5072f3202a3e82d17d6916eb9d689fc
jiyoungKim <rlawldud.kim99@gmail.com>
2023-06-20 15:06:42 +0900

    6/20 codingtest practice

---
+++ bj15655.py
@@ -0,0 +1,21 @@
+def dfs(i, temp):
+    temp.append(nums[i])
+    if len(temp) == m:
+        print(" ".join(map(str, temp)))
+        return
+    
+    for j in range(i+1, n):
+        dfs(j, temp)
+        temp.pop()
+        
+    
+    
+    
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+for i in range(n):
+    temp = []
+    dfs(i, temp)
