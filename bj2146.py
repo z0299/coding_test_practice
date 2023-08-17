216f5a3c774280c481225fb8f49867cb81e2a618
jiyoungKim <rlawldud.kim99@gmail.com>
2023-08-17 22:11:20 +0900

    8/17 codingtest practice

--- bj2146.py
+++ bj2146.py
@@ -4,7 +4,6 @@ from collections import deque
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0]*n for _ in range(n)]
-# print(board)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
@@ -12,6 +11,15 @@ def bfs(x, y):
     side = set()
     q = deque()
     q.append((x, y))
+    flag = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
+                flag = 1
+    if flag == 1:
+        side.add((x, y))
+        
     while q:
         x, y = q.popleft()
         visited[x][y] = 1
@@ -26,11 +34,21 @@ def bfs(x, y):
                     if 0 <= nnx < n and 0 <= nny < n and board[nnx][nny] == 0:
                         side.add((nx, ny))
     return side
-
             
 islands = []
 for x in range(n):
     for y in range(n):
-        if board[x][y] == 1:
+        if board[x][y] == 1 and not visited[x][y]:
             temp = bfs(x, y)
-            print(temp)
+            islands.append(list(temp))
+            
+answer = 1e9
+for i in range(0, len(islands)-1):
+    for j in range(i+1, len(islands)):
+        for c1 in islands[i]:
+            for c2 in islands[j]:
+                temp_val = (abs(c2[0] - c1[0]) + abs(c2[1] - c1[1])) - 1
+                if temp_val < answer:
+                    answer = temp_val
+
+print(answer)
