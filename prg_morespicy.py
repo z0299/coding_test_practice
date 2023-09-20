683b6c4fc74024303fc1eaa6fff8f27e79b6b64e
jiyoungKim <rlawldud.kim99@gmail.com>
2023-09-20 23:45:16 +0900

    9/20 codingtest practice

--- prg_morespicy.py
+++ prg_morespicy.py
@@ -9,7 +9,7 @@ def solution(scoville, K):
         heapq.heappush(scoville, mix)
         answer += 1
         
-        if len(scoville) == 1 and scoville[0] < K:
+        if len(scoville) == 1 and scoville[0] < K:  # 음식이 1개 남을 때까지 합쳤는데, 최소가 K보다 작은 경우
             return -1
     
     return answer 
